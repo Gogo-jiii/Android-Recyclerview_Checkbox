e6805d537bdd79509841f900949a44a883a6d7b1
vaibhav <vgion.tech@gmail.com>
2021-04-09 19:17:41 +0530

    init

---
+++ app/src/main/java/com/example/recyclerviewcheckbox/MyAdapter.java
@@ -0,0 +1,83 @@
+package com.example.recyclerviewcheckbox;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<ModelClass> arrayList;
+
+    public MyAdapter(MainActivity context, ArrayList<ModelClass> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.row_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ModelClass modelClass = arrayList.get(position);
+        holder.textView.setText(modelClass.getName());
+        holder.checkBox.setChecked(modelClass.isSelected());
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textView;
+        private CheckBox checkBox;
+        private ConstraintLayout rowItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.textView = itemView.findViewById(R.id.textView);
+            this.rowItem = itemView.findViewById(R.id.rowitem);
+            this.checkBox = itemView.findViewById(R.id.checkBox);
+
+            checkBox.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    boolean isChecked = ((CheckBox) v).isChecked();
+
+                    if (isChecked) {
+                        arrayList.get(getAdapterPosition()).setSelected(true);
+                    } else {
+                        arrayList.get(getAdapterPosition()).setSelected(false);
+                    }
+                }
+            });
+
+            rowItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, String.valueOf(arrayList.get(getAdapterPosition())),
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
